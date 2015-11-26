cd3171ce4bc35d3db0b43acb5307f39cfc03c564
Renddi <renddi@relab.cc>
2015-11-26 18:00:49 +0800

    Initial commit

---
+++ src/js/states/preloader.js
@@ -0,0 +1,39 @@
+var _ = require('lodash');
+var Preloader = function (game) {
+  this.asset = null;
+  this.ready = false;
+};
+
+module.exports = Preloader;
+
+Preloader.prototype = {
+
+  preload: function () {
+    var menuJson = require('../json/menu');
+    _.forEach(menuJson.layers, function(layer) {
+      this.load.image('menu-' + layer.name, 'assets/menu/' + layer.filename);
+      console.log('menu-' + layer.name, 'assets/menu/' + layer.filename);
+    });
+
+    this.asset = this.add.sprite(320, 240, 'preloader');
+    this.asset.anchor.setTo(0.5, 0.5);
+
+    this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
+    this.load.setPreloadSprite(this.asset);
+    this.load.image('testsprite', 'assets/test.png');
+  },
+
+  create: function () {
+    this.asset.cropEnabled = false;
+  },
+
+  update: function () {
+    if (!!this.ready) {
+      this.game.state.start('Menu');
+    }
+  },
+
+  onLoadComplete: function () {
+    this.ready = true;
+  }
+};
