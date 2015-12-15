872134bedaa4c0ed3f6b7f4a5eb9c5f830988f1e
Renddi <renddi@RenddideMacBook-Air.local>
2015-12-15 12:37:38 +0800

    adjust game display on small laptop

--- src/js/states/boot.js
+++ src/js/states/boot.js
@@ -12,22 +12,15 @@ Boot.prototype = {
         this.input.maxPointers = 1;
         this.stage.disableVisibilityChange = true;
 
-        if (this.game.device.desktop)
-        {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(320, 504, 600, 945);
-            this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.setMinMax(320, 504, 600, 945);
-            this.scale.pageAlignHorizontally = true;
-            this.scale.pageAlignVertically = true;
-            this.scale.forceOrientation(true, false);
-            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
-        }
+
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.setMinMax(320, 504, 600, 945);
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.scale.forceOrientation(true, false);
+        this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+        this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+    
 
         // this.scale.setResizeCallback(this.gameResized, this);
     },
